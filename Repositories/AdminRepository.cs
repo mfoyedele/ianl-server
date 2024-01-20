1b5ab01b27eed39f9fca94f73f1895428b508e65
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-01-20 16:44:44 +0100

    Update AdminRepository

--- Repositories/AdminRepository.cs
+++ Repositories/AdminRepository.cs
@@ -7,24 +7,45 @@ using WebApi.Helpers;
 public interface IAdminRepository
 {
     Task<IEnumerable<Admin>> GetAll();
-    Task<User> GetById(int id);
-    Task<User> GetByEmail(string email);
+    Task<Admin> GetById(int id);
+    Task<Admin> GetByEmail(string email);
 }
 
 public class AdminRepository : IAdminRepository
 {
-    public Task<IEnumerable<Admin>> GetAll()
+    private DataContext _context;
+
+    public AdminRepository(DataContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
     }
 
-    public Task<User> GetByEmail(string email)
+    public async Task<IEnumerable<Admin>> GetAll()
     {
-        throw new NotImplementedException();
+        using var connection = _context.CreateConnection();
+        var sql = """
+            SELECT * FROM Admins
+        """;
+        return await connection.QueryAsync<Admin>(sql);
+    }
+public async Task<Admin> GetById(int id)
+    {
+        using var connection = _context.CreateConnection();
+        var sql = """
+            SELECT * FROM Users 
+            WHERE Id = @id
+        """;
+        return await connection.QuerySingleOrDefaultAsync<Admin>(sql, new { id });
     }
 
-    public Task<User> GetById(int id)
+   public async Task<Admin> GetByEmail(string email)
     {
-        throw new NotImplementedException();
+        using var connection = _context.CreateConnection();
+        var sql = """
+            SELECT * FROM Admins 
+            WHERE Email = @email
+        """;
+        return await connection.QuerySingleOrDefaultAsync<Admin>(sql, new { email });
     }
-}
+
+ }
