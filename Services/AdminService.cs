cfddfb97070244d85840e6b7165413546ebde490
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-01-20 17:26:31 +0100

    Update AdminService

--- Services/AdminService.cs
+++ Services/AdminService.cs
@@ -12,3 +12,31 @@ public interface IAdminService
     Task Update(int id, UpdateRequest model);
     Task Delete(int id);
 }
+
+public class AdminService : IAdminService
+{
+    public Task Create(CreateRequest model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Admin>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Admin> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(int id, UpdateRequest model)
+    {
+        throw new NotImplementedException();
+    }
+}
